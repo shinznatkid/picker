e15a01bfc6c791e82cf023073b32116fe5ea95b7
shinznatkid <shinznatkid@gmail.com>
2014-05-08 04:32:36 +0700

    - Cleanup

--- setup.py
+++ setup.py
@@ -1,24 +1,29 @@
 # -*- coding: utf-8 -*-
 # from distutils.core import setup
+import os
+import sys
 from setuptools import setup, find_packages
+from distutils.sysconfig import get_python_lib
 
-version = '0.1.0'
+version = '0.1.2'
 
 setup(
     name='django-picker',
     version=version,
     author=u'ShinZ Natkid',
     author_email='shinznatkid@gmail.com',
-    packages=find_packages(), #['picker'],
+    packages=find_packages(),
     url='http://github.com/shinznatkid/picker',
     license='BSD',
     include_package_data=True,
     description='Put javascript, css framework into your project.',
     long_description=open('README.md').read(),
-    # package_data={
-    #     'picker': ['static/*'],
-    # },
+    zip_safe=False,
     install_requires=[
         "django",
     ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Programming Language :: Python :: 2.7',
+    ]
 )
